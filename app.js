4484c9ec2fc7375982034aee4b18fe12f10c0cef
atolat <atolat@uci.edu>
2017-05-25 16:07:58 -0500

    Test

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ var PORT = process.env.PORT || 3000;
 const express = require('express');
 const router = express.Router();
 const pg = require('pg');
-const connectionString ='postgres://pdolcdhpesaiuw:a5c1df771d3b0b2b7554e4cf06621980cd0d1a647bd155fb6ae3848ce2856120@ec2-174-129-224-33.compute-1.amazonaws.com:5432/d6qes2ci89d25b';
+var app = express();
+
+    //const conString = 'postgres://pdolcdhpesaiuw:a5c1df771d3b0b2b7554e4cf06621980cd0d1a647bd155fb6ae3848ce2856120@ec2-174-129-224-33.compute-1.amazonaws.com:5432/d6qes2ci89d25b'
 
 
-
-allowCrossDomain = function (req, res, next) {
+allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -19,36 +20,55 @@ allowCrossDomain = function (req, res, next) {
 
 app.use(allowCrossDomain);
 
+var client = new pg.Client({
+    user: "pdolcdhpesaiuw",
+    password: "a5c1df771d3b0b2b7554e4cf06621980cd0d1a647bd155fb6ae3848ce2856120",
+    database: "d6qes2ci89d25b",
+    port: 5432,
+    host: "ec2-174-129-224-33.compute-1.amazonaws.com",
+    ssl: true
+});
+client.connect();
 
-    
 router.get('/getdata', (req, res, next) => {
-  const results = [];
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Select Data
-    console.log('connected...');
-    const query = client.query('SELECT * FROM public.LNA_ODS_FEEDBACK_ACTION;');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
+    const results = [];
+    //   // Get a Postgres client from the connection pool
+    //   pg.connect(connectionString, (err, client, done) => {
+    //     // Handle connection errors
+    //     if(err) {
+    //       done();
+    //       console.log(err);
+    //       return res.status(500).json({success: false, data: err});
+    //     }
+    //     // SQL Query > Select Data
+    //     const query = client.query('SELECT * FROM public.LNA_ODS_CN_ACTION;');
+    //     // Stream results back one row at a time
+    //     query.on('row', (row) => {
+    //       results.push(row);
+    //     });
+    //     // After all data is returned, close connection and return results
+    //     query.on('end', () => {
+    //       done();
+    //       return res.json(results);
+    //     });
+    //   });
+    // });
+
+    var query = client.query("SELECT * FROM public.LNA_ODS_FEEDBACK_ACTION");
+    query.on("row", function(row, result) {
+        result.addRow(row);
     });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
+    query.on("end", function(result) {
+        console.log(JSON.stringify(result.rows, null, "    "));
+        return res.json(result);
+        client.end();
     });
-  });
 });
 
 
+
 //Connect to Express
-app.listen(PORT, function () {
+app.listen(PORT, function() {
     console.log('Express listening on port:: ' + PORT);
 
 });
--- routes/server.js
+++ routes/server.js
@@ -11,26 +11,36 @@ const connectionString = process.env.DATABASE_URL || 'postgres://pdolcdhpesaiuw:
     
 router.get('/getdata', (req, res, next) => {
   const results = [];
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, (err, client, done) => {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Select Data
-    const query = client.query('SELECT * FROM public.LNA_ODS_CN_ACTION;');
-    // Stream results back one row at a time
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', () => {
-      done();
-      return res.json(results);
-    });
-  });
+//   // Get a Postgres client from the connection pool
+//   pg.connect(connectionString, (err, client, done) => {
+//     // Handle connection errors
+//     if(err) {
+//       done();
+//       console.log(err);
+//       return res.status(500).json({success: false, data: err});
+//     }
+//     // SQL Query > Select Data
+//     const query = client.query('SELECT * FROM public.LNA_ODS_CN_ACTION;');
+//     // Stream results back one row at a time
+//     query.on('row', (row) => {
+//       results.push(row);
+//     });
+//     // After all data is returned, close connection and return results
+//     query.on('end', () => {
+//       done();
+//       return res.json(results);
+//     });
+//   });
+// });
+
+var query = client.query("SELECT * FROM public.LNA_ODS_FEEDBACK_ACTION");
+query.on("row", function (row, result) {
+    result.addRow(row);
+});
+query.on("end", function (result) {
+    console.log(JSON.stringify(result.rows, null, "    "));
+    return res.json(result);
+    client.end();
 });
 
     
